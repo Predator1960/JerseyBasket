3b16544115be4b6a569167bb2d05e361cbca0b70
Predator1960 <eamonn1960@yahoo.co.uk>
2026-07-02 15:56:57 +0100

    v79: 1185 products

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs => regs.forEach(r => r.unregister()));
-}
